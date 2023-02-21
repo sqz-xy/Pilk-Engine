a883af45c13903a68f092b7146388ceefdc5447d
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2023-02-21 17:39:22 +0000

    Added camera class and updated added logic to move/update it. Doesn't seem to work yet.

--- src/Engine/Managers/SceneManager.cpp
+++ src/Engine/Managers/SceneManager.cpp
@@ -76,6 +76,8 @@ int SceneManager::Run()
     float dt;
     auto lastTime = static_cast<float>(glfwGetTime());
 
+    glDepthMask(GL_TRUE); // Depth mask
+
     // Simple update loop
     while (!glfwWindowShouldClose(window))
     {
@@ -93,7 +95,7 @@ int SceneManager::Run()
         ImGui::NewFrame();
 
         // Input processing
-        processInput(window);
+        SceneManager::processInput(window, dt);
 
         // Current scene render/update
         SceneManager::Render(dt);
@@ -184,11 +186,9 @@ void SceneManager::Update(const float p_dt)
 /// Input processing
 /// </summary>
 /// <param name="p_window">The current window</param>
-void SceneManager::processInput(GLFWwindow* p_window)
+void SceneManager::processInput(GLFWwindow* p_window, const float p_dt)
 {
-    // Exit if escape key is pressed
-    if (glfwGetKey(p_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(p_window, true);
+    m_currentScene->ProcessInput(p_window, p_dt);
 }
 
 
--- src/Engine/Utility/Camera.h
+++ src/Engine/Utility/Camera.h
@@ -10,7 +10,33 @@ class Camera final
 {
 public:
 
+	enum Direction
+	{
+		Forward,
+		Backward,
+		Left,
+		Right
+	};
+
+	explicit Camera(const glm::vec3 p_pos, const glm::vec3 p_target, const float p_sens, const int p_vpWidth, const int p_vpHeight);
+	~Camera();
+	
+	void MoveCamera(const Direction p_direction, const float p_distance, const float p_dt);
+	void UpdateCamera(const int p_shaderHandle);
+
+	glm::mat4 m_view;
+	glm::mat4 m_proj;
+
 	glm::vec3 m_cameraPos;
-	glm::vec3 m_cameraTarget;
+	const glm::vec3 m_globalUp = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 m_cameraRight;
 	glm::vec3 m_cameraDirection;
+
+private:
+
+	glm::vec2 m_lastMousePos;
+	float m_pitch;
+	float m_yaw;
+	float m_sensitivity;
+	bool m_mouseMoved;
 };
