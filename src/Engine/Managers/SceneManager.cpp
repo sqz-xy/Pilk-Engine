f0eb3f4b4f721015affde7e45f97f5e2ca90e9f8
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2023-02-23 18:58:00 +0000

    Changed vertex binding

--- src/Engine/Managers/SceneManager.cpp
+++ src/Engine/Managers/SceneManager.cpp
@@ -35,8 +35,8 @@ SceneManager::~SceneManager()
 int SceneManager::Run() 
 {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create the window
--- src/Engine/Scenes/MainMenuScene.cpp
+++ src/Engine/Scenes/MainMenuScene.cpp
@@ -29,20 +29,6 @@ public:
 	Camera* m_Camera;
 
 
-
-	float m_vertices[72]
-	{
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
-	};
-
-	float m_indices[39]
-	{
-		0, 1, 2
-	};
-
-
 	explicit MainMenuScene(SceneManager* pSceneManager) : Scene(pSceneManager)
 	{
 		m_sceneManager->m_windowName = "MainMenuScene";
@@ -78,8 +64,9 @@ public:
 		m_Camera->UpdateCamera(m_shaderProgramID);
 
 
-		glBindVertexArray(m_vao);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(m_vao); 
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		ImVec2 vec(100, 50);
 		//std::cout << "Rendering" << std::endl;
@@ -106,33 +93,44 @@ public:
 
 	void Load() override
 	{
-		std::cout << "Scene Loaded" << std::endl;
+		float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+	   -0.5f, -0.5f, 0.0f,  // bottom left
+	   -0.5f,  0.5f, 0.0f   // top left 
+		};
 
-		// TODO: Refactor vertices and indices into geometry component, Refactor shader into shader component
-		
+		unsigned int indices[] = {  
+			0, 1, 3,  // first Triangle
+			1, 2, 3   // second Triangle
+		};
 
 		unsigned int VBO, VAO, EBO;
-		glGenBuffers(1, &VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices), m_vertices, GL_STATIC_DRAW);
-
-		glGenBuffers(1, &EBO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_indices), m_indices, GL_STATIC_DRAW);
 
 		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
 
 		// Creates a shader program
 		unsigned int shaderProgram;
 		if (!ResourceManager::CreateShaderProgram(&shaderProgram, "resources/shaders/VertexShader.vert", "resources/shaders/FragmentShader.frag")) return;
 
-		// Buffer Data
-		glBindVertexArray(VAO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices), m_vertices, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-		glEnableVertexAttribArray(0);
-
+		
 		m_vao = VAO;
 		m_shaderProgramID = shaderProgram;	
 	}
