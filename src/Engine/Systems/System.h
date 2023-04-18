8ae9f9eb1064fdd60c6ab59a3619f49cfc0d7a1a
mattliney <M.LINEY-2020@hull.ac.uk>
2023-04-18 13:04:05 +0100

    delete

--- src/Engine/Systems/System.h
+++ src/Engine/Systems/System.h
@@ -157,28 +157,3 @@ public:
 private:
 	Camera* m_camera;
 };
-
-/// <summary>
-/// Render system by Matthew Liney
-/// </summary>
-class SystemPhysics : public System
-{
-public:
-	SystemPhysics() {}
-
-	virtual void Execute(const float p_deltaTime) override
-	{
-		for each (Entity * entity in validEntities)
-		{
-
-		}
-	}
-
-	virtual void ValidateEntity(Entity* p_entity) override
-	{
-		bool valid = p_entity->GetComponent<ComponentVelocity>() != nullptr &&
-					 p_entity->GetComponent<ComponentTransform>() != nullptr;
-
-		System::ValidateEntity(p_entity, valid);
-	}
-};
